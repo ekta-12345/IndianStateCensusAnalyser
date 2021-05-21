c0303f7e738f2fe889d384c9a3e7550682e9ee97
ekta12345 <ektakumbhare3@gmail.com>
2021-05-21 08:44:46 +0530

    Added: Test Cases to ensure if the number of records match or throw a custom exception

--- IndianStateCensusTest/UnitTest1.cs
+++ IndianStateCensusTest/UnitTest1.cs
@@ -14,13 +14,19 @@ namespace IndianStateCensusTest
         static string InvalidCSVTypeFilePath = @"D:\C#\IndianStateCensusAnalyser\CSVFiles\CensorAnalyser.cs";
         static string InvalidDeliminatorFilePath = @"D:\C#\IndianStateCensusAnalyser\CSVFiles\IncorrectDeliminatorCensusFile.csv";
         static string InvalidHeaderFilePath = @"D:\C#\IndianStateCensusAnalyser\CSVFiles\DelimiterIndiaStateCode.csv";
+        //UC2
+        static string CSVStateCodeFilePath = @"D:\Practice\C#\Indian States Census Analyser Problem\Indian States Census Analyser Problem\CSVFiles\IndiaStateCode.csv";
+        static string InvalidDeliminatorStateCodeFilePath = @"D:\Practice\C#\Indian States Census Analyser Problem\Indian States Census Analyser Problem\CSVFiles\InvalidDeliminatorIndiaStateCode.csv";
+
+        static string StateCensusFileHeaders = "State,Population,AreaInSqKm,DensityPerSqKm"; //file header
+        static string StateCodeFileHeaders = "SrNo,State Name,TIN,StateCode";//state code file header
 
         CensusAnalyser censusAnalyser;
         CSVFileData csvFileData;
         CSVFactory csvFactory;
         List<string> totalNumberOfRecords;
 
-        public string StateCensusFileHeaders { get; private set; }
+        
 
         [SetUp]
         public void Setup()
@@ -119,5 +125,90 @@ namespace IndianStateCensusTest
                 Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_HEADERS, ex.type);
             }
         }
+        // 2.1:- From the given States Census CSV file, Check and ensure if the number of record matches.
+                
+        [Test]
+        public void FromGivenStateCensusCSVFile_IfCorrect_ShouldReturnNoOfRecords()
+        {
+
+            CensusAnalyser censusAnalyser = (CensusAnalyser)csvFactory.getIndianCensusAnalyser();
+            csvFileData = new CSVFileData(censusAnalyser.LoadCSVFileData);
+            totalNumberOfRecords = (List<string>)csvFileData(CSVStateCodeFilePath, StateCodeFileHeaders);
+            Assert.AreEqual(37, totalNumberOfRecords.Count);
+        }
+
+        // TC2.2:-From the given State Census CSV File, if incorrect Returns a custom Exception.
+                 
+        
+        [Test]
+        public void FromGivenStateCodesCSVFile_IfFileNotFound_ShouldThrowCustomException()
+        {
+            try
+            {
+
+                CensusAnalyser censusAnalyser = (CensusAnalyser)csvFactory.getIndianCensusAnalyser();
+                csvFileData = new CSVFileData(censusAnalyser.LoadCSVFileData);
+                var exception = Assert.Throws<CensusAnalyserException>(() => csvFileData(InvalidFilePath, StateCodeFileHeaders));
+
+            }
+            catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(CensusAnalyserException.ExceptionType.FILE_NOT_FOUND, ex.type);
+            }
+        }
+        // TC2.3:-From given State Census CSV File, if correct but type is incorrect should return a custom Exception.
+               
+       
+        [Test]
+        public void FromGivenStateCodesCSVFile_IfIncorrectFileType_ShouldThrowCustomException()
+        {
+            try
+            {
+
+                CensusAnalyser censusAnalyser = (CensusAnalyser)csvFactory.getIndianCensusAnalyser();
+                csvFileData = new CSVFileData(censusAnalyser.LoadCSVFileData);
+                var exception = Assert.Throws<CensusAnalyserException>(() => csvFileData(InvalidCSVTypeFilePath, StateCodeFileHeaders));
+            }
+            catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(CensusAnalyserException.ExceptionType.INCORRECT_FILE_TYPE, ex.type);
+            }
+
+        }
+        // TC2.4:- From the given State Census CSV File, if correct but delimiter is incorrect should return a custom Exception.
+                 
+        [Test]
+        public void FromGivenStateCodesCSVFile_IfIncorrectDeliminatorPresentInFile_ShouldThrowCustomException()
+        {
+            try
+            {
+                CensusAnalyser censusAnalyser = (CensusAnalyser)csvFactory.getIndianCensusAnalyser();
+                csvFileData = new CSVFileData(censusAnalyser.LoadCSVFileData);
+                var exception = Assert.Throws<CensusAnalyserException>(() => csvFileData(InvalidDeliminatorStateCodeFilePath, StateCodeFileHeaders));
+            }
+            catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_DELIMITER, ex.type);
+            }
+        }
+        // TC2.5:- From the given the State Census CSV File ,if correct but csv header incorrect should return a custom Exception.
+                  
+         
+        [Test]
+        public void FromGivenStateCodesCSVFile_IfIncorrectHeadersPresentInFile_ShouldThrowCustomException()
+        {
+            try
+            {
+                CensusAnalyser censusAnalyser = (CensusAnalyser)csvFactory.getIndianCensusAnalyser();
+                csvFileData = new CSVFileData(censusAnalyser.LoadCSVFileData);
+                var exception = Assert.Throws<CensusAnalyserException>(() => csvFileData(InvalidHeaderFilePath, StateCodeFileHeaders));
+            }
+            catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_HEADERS, ex.type);
+            }
+
+        }
     }
 }
+    
